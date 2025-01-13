48a9fe6d752273c721639b9da2ff3a0444e57b92
Onè <43485962+c-git@users.noreply.github.com>
2025-01-13 04:00:06 +0000

    Expose Runtime to see if it provides the impls

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -9,7 +9,7 @@ mod alpha;
 /// Built-in plugins
 mod plugins;
 
-pub use alpha::{start, Alpha, RuntimeServer};
+pub use alpha::{Runtime, start, Alpha, RuntimeServer};
 
 // Public API
 pub use plugins::{Metadata, Secrets};
