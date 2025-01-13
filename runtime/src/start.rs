56c038119a78951396e61a710d4fda1dfcd77a43
Onè <43485962+c-git@users.noreply.github.com>
2025-01-13 02:41:29 +0000

    Remove most of start function

--- runtime/src/start.rs
+++ runtime/src/start.rs
@@ -49,54 +49,7 @@ impl Args {
 }
 
 pub async fn start(loader: impl Loader + Send + 'static, runner: impl Runner + Send + 'static) {
-    // `--version` overrides any other arguments. Used by cargo-shuttle to check compatibility on local runs.
-    if std::env::args().any(|arg| arg == "--version") {
-        println!("{}", crate::VERSION_STRING);
-        return;
-    }
-
-    println!("{} starting", crate::VERSION_STRING);
-
-    let args = match Args::parse() {
-        Ok(args) => args,
-        Err(e) => {
-            eprintln!("ERROR: Runtime failed to parse args: {e}");
-            let help_str = "[HINT]: Run your Shuttle app with `shuttle run` or `cargo shuttle run`";
-            let wrapper_str = "-".repeat(help_str.len());
-            eprintln!("{wrapper_str}\n{help_str}\n{wrapper_str}");
-            return;
-        }
-    };
-
-    // this is handled after arg parsing to not interfere with --version above
-    #[cfg(feature = "setup-tracing")]
-    {
-        use tracing_subscriber::{fmt, prelude::*, registry, EnvFilter};
-        registry()
-            .with(fmt::layer().without_time())
-            .with(
-                // let user override RUST_LOG in local run if they want to
-                EnvFilter::try_from_default_env()
-                    // otherwise use our default
-                    .or_else(|_| {
-                        EnvFilter::try_new(if args.beta {
-                            "info"
-                        } else {
-                            "info,shuttle=trace"
-                        })
-                    })
-                    .unwrap(),
-            )
-            .init();
-
-        if args.beta {
-            tracing::warn!(
-                "Default tracing subscriber initialized (https://docs.shuttle.dev/docs/logs)"
-            );
-        } else {
-            tracing::warn!("Default tracing subscriber initialized (https://docs.shuttle.rs/configuration/logs)");
-        }
-    }
+    let args: Args = todo!();
 
     if args.beta {
         rt::start(loader, runner).await
